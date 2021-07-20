25f4c1ddefec1c9af41dbacb3984698bb35297de
Jessica Stern <jlstern3@gmail.com>
2021-07-20 13:30:48 -0400

    Question 2 answered correctly

--- practice.js
+++ practice.js
@@ -11,6 +11,15 @@ function return_arr(){
 
 // 2. Write a function that would get the sum of all the even numbers from 1 to 1000. You may use a modulus operator for this exercise.
 
+function sum_even(){
+    var sum = 0;
+    for(var i = 1; i <1001; i++){
+        if (i%2 == 0){
+            sum+=i;
+        }
+    }
+    return sum;
+}
 // 3. Write a function that returns the sum of all the odd numbers from 1 to 5000. (e.g. 1+3+5+...+4997+4999)
 
 // 4. Write a function that returns the sum of all the values within an array. (e.g. [1,2,5] returns 8, [-5,2,5,12] returns 14)
