2854d42be33f71f651e38b363d0e4799fdf2cde0
Jessica Stern <jlstern3@gmail.com>
2021-07-21 21:49:57 -0400

    Question 8 answered correctly

--- practice.js
+++ practice.js
@@ -70,7 +70,15 @@ function odd_arr(){
     return arr;
 }
 // 8. Given value of Y, write a function that takes an array and returns the number of values that are greater than Y. For example if arr = [1, 3, 5, 7] and Y = 3, your function will return 2. (There are two values in the array greater than 3, which are 5, 7).
-
+function greater_than_Y(arr, Y){
+    var count = 0;
+    for (var i = 0; i < arr.length; i++){
+        if(arr[i]>Y){
+            count +=1;
+        }
+    }
+    return count;
+}
 // 9. Given an array with multiple values, write a function that replaces each value in the array with the product of the original value multiplied by itself. (e.g. [1,5,10,-2] will become [1,25,100,4])
 
 // 10. Given an array with multiple values, write a function that replaces any negative numbers within the array with the value of 0. When the program is done the array should contain no negative values. (e.g. [1,5,10,-2] will become [1,5,10,0])
