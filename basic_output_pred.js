9d5059da6c7d2c3b452c069ccd8cbc498c2ec83d
Jessica Stern <jlstern3@gmail.com>
2021-07-23 22:19:42 -0400

    Question 5 answered correctly

--- basic_output_pred.js
+++ basic_output_pred.js
@@ -24,12 +24,13 @@ function y(num1, num2){
 console.log(y(2,3));
 console.log(y(3,5));
 
-// 5. function y(num1, num2){
-//     console.log(num1);   
-//     return num1+num2;
-//  }
-//  console.log(y(2,3));
-//  console.log(y(3,5));
+// 5. Output: 2,5,3,8
+function y(num1, num2){
+    console.log(num1);   
+    return num1+num2;
+}
+console.log(y(2,3));
+console.log(y(3,5));
 
 // 6. a = 15;
 // console.log(a);
