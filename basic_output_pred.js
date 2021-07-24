e071d132e2ee4655f38f675276ee08fc10688b32
Jessica Stern <jlstern3@gmail.com>
2021-07-23 22:27:56 -0400

    Assignment correctly completed

--- basic_output_pred.js
+++ basic_output_pred.js
@@ -43,15 +43,13 @@ b = y(10);
 console.log(b);
 
 
-// 7. a = 15;
-// console.log(a);
-// function y(a){
-//    console.log(a);   
-//    return a*2;
-// }
-// b = y(10);
-// console.log(b);
-
-
-8. 
+// 7. Output: 15,10,20
+a = 15;
+console.log(a);
+function y(a){
+    console.log(a);   
+    return a*2;
+}
+b = y(10);
+console.log(b);
 
