af37660f2134a6500367df94119cd4b4d4fe0b80
Jessica Stern <jlstern3@gmail.com>
2021-07-23 21:59:59 -0400

    Question 12 answered correctly

--- algo_app.js
+++ algo_app.js
@@ -115,5 +115,10 @@ function max_min_avg(arr){
     return new_arr;  
 }
 // 12. Write a function that will swap the first and last values of any given array. The default minimum length of the array is 2. (e.g. [1,5,10,-2] will become [-2,5,10,1]).
-
+function swap_first_last(arr){
+    var temp = arr[0];
+    arr[0]=arr[arr.length-1];
+    arr[arr.length-1] = temp;
+    return arr;
+}
 // 13. Write a function that takes an array of numbers and replaces any negative values within the array with the string 'Dojo'. For example if array = [-1,-3,2], your function will return ['Dojo','Dojo',2].
