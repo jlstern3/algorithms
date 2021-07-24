f2aaaeeb468d1fd8d9053f5d38242f57fc286232
Jessica Stern <jlstern3@gmail.com>
2021-07-23 22:01:39 -0400

    All questions complete and correct

--- algo_app.js
+++ algo_app.js
@@ -122,3 +122,11 @@ function swap_first_last(arr){
     return arr;
 }
 // 13. Write a function that takes an array of numbers and replaces any negative values within the array with the string 'Dojo'. For example if array = [-1,-3,2], your function will return ['Dojo','Dojo',2].
+function dojo(arr){
+    for(var i = 0; i<arr.length; i++){
+        if (arr[i]<0){
+            arr[i] = "Dojo";
+        }
+    }
+    return arr;
+}
