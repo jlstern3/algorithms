85489030dda159a15bf30308aa3e1f4116796aa4
Jessica Stern <jlstern3@gmail.com>
2021-07-24 21:07:30 -0400

    All questions established, unanswered.

---
+++ code_paper_algos.js
@@ -0,0 +1,16 @@
+// 1. Print 1 to x
+// Please complete the codes below to have the function print all the integers from 1 to x.  
+// If x is negative, have it print/log "negative number' and have the function return false.
+
+// 2. PrintSum
+// Please complete the code below to have the function print integers from 0 to 255 and with each integer print the sum so far.  
+// Have the function return the final sum
+
+// 3. PrintSumArray
+// Please complete the code below to have the function return the sum of all the values in a given array
+
+// 4. Analyze an array’s values and return the average of each respective array.
+
+// 5. Create an array with all the odd integers between 1 and 255 (inclusive)
+
+// 6. Square each value in a given array, returning that same array with changed values.
