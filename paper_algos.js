23c7be1aecfbd4fcfd681c8807cdcdd13ac3f80f
Jessica Stern <jlstern3@gmail.com>
2021-07-24 20:56:53 -0400

    Question 7 answered correctly

--- paper_algos.js
+++ paper_algos.js
@@ -61,7 +61,7 @@ function foo(x,y){
 var z = foo(3,3);
 console.log(z);
 
-7. 
+// 7. Output: 0,0,0,0,0,0,1,2,3,4,0,2,4,6,8,15
 function foo(x,y){
     for(var i=0; i<x; i++){
         for(var j=0; j<y; j++){        
