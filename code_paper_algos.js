461bbdf81fac7f2e507369faa22e5af63a8569df
Jessica Stern <jlstern3@gmail.com>
2021-07-24 21:19:49 -0400

    Question 1 answered correctly

--- code_paper_algos.js
+++ code_paper_algos.js
@@ -1,6 +1,20 @@
 // 1. Print 1 to x
 // Please complete the codes below to have the function print all the integers from 1 to x.  
 // If x is negative, have it print/log "negative number' and have the function return false.
+function print_to_x(x){
+    if(x<0){
+        console.log("negative number");
+        return false;
+    }
+    else{
+        for(var i = 1; i < x+1; i++){
+            console.log(i);
+        }
+    }
+}
+
+
+
 
 // 2. PrintSum
 // Please complete the code below to have the function print integers from 0 to 255 and with each integer print the sum so far.  
