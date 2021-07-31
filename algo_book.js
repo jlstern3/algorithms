57a1da778818941e2bbbab6489285be97b57cb90
Jessica Stern <jlstern3@gmail.com>
2021-07-31 16:22:43 -0400

    6 questions established, unanswered

---
+++ algo_book.js
@@ -0,0 +1,27 @@
+// 1. Create a function that returns an array with multiple of 3 up to 999 (pg 28)
+
+// 2. Implement a function that accepts two non-negative integers as arguments 
+//ex: lastDigitAtoB(a,b) should return the last digit of the first number (a) raised to an exponent of the second number (b)
+//pg 43
+
+// 3. Given an array and an additional value, insert this
+// value at the beginning of the array. Do this
+// without using any built-in array methods. (pg 52)
+
+// 4. Given an array, index, and additional value, insert
+// the value into the array at the given index. Do this
+// without using built-in array methods. You can
+// think of PushFront(arr,val) as equivalent to
+// InsertAt(arr,0,val).
+
+// 5. RemoveAt
+// Given an array and an index into the array,
+// remove and return the array value at that index.
+// Do this without using any built-in array methods
+// except pop(). Think of PopFront(arr) as
+// equivalent to RemoveAt(arr,0).
+
+// 6. PopFront
+// Given an array, remove and return the value at
+// the beginning of the array. Do this without using
+// any built-in array methods except pop().
