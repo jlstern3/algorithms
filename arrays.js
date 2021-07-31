bebd50e34fbd0a50592a1179df2a48d04d9ccfe8
Jessica Stern <jlstern3@gmail.com>
2021-07-31 16:17:55 -0400

    Algo Book file created

--- arrays.js
+++ arrays.js
@@ -56,6 +56,7 @@ function removeNegatives(arr){
 // 6. Given an array, move all values forward by one index, dropping the first and leaving a ‘0’ value at the end.  
 // For example moveForward( [1,2,3]) should return [2,3,0].
 
+
 // 7. Given an array, return an array with values in a reversed order.  
 // For example, returnReversed([1,2,3]) should return [3,2,1].
 
