8e4946fe582ac777572794af8edb26faa14c21ea
Jessica Stern <jlstern3@gmail.com>
2021-08-01 18:01:59 -0400

    Questions 7-11 added, unanswered

--- algo_book.js
+++ algo_book.js
@@ -25,3 +25,34 @@
 // Given an array, remove and return the value at
 // the beginning of the array. Do this without using
 // any built-in array methods except pop().
+
+// 7. Reverse Array
+// Given a numerical array, reverse the order of the
+// values. The reversed array should have the same
+// length, with existing elements moved to other
+// indices so that the order of elements is reversed.
+
+// 8. Remove Negatives
+// Implement a function removeNegatives() that
+// accepts an array and removes any values that
+// are less than zero.
+// Second-level challenge: don’t use nested loops.
+
+// 9. Skyline Heights
+// You are given an array with heights of consecutive buildings in the city. For example, [-1,7,3] would
+// represent three buildings: first is actually below street level, second is seven stories high, and third is
+// three stories high (but hidden behind the seven-story onbe). You are situated at street level. Return an
+// array containing heights of the buildings you can see, in order. Given [1,-1,7,3] return [1,7].
+
+// 10. Binary Search
+// Given a sorted array and a value, return whether
+// that value is present in the array. Do not
+// sequentially iterate the entire array. Instead,
+// ‘divide and conquer’, taking advantage of the fact
+// that the array is sorted.
+
+// 11. Min Of Sorted-Rotated
+// You will be given a numerical array that has first
+// been sorted, then rotated by an unknown
+// amount. Find and return the minimum value in
+// that array
